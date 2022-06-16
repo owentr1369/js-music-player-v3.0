d05ab6eb4387ac6fd753b514eb3de547a54a7e11
owentr1369 <tamvaa1306@gmail.com>
2022-06-16 09:43:58 +0700

    add music

---
+++ music/music.js
@@ -0,0 +1,62 @@
+export const songs = [
+    {
+      name: "okokok",
+      singer: "Low G",
+      path: "./mp3/okeokeoke - Low G.mp3",
+      image: "./store/music/thumbnail/okokok.jpg",
+    },
+    {
+      name: "Tam Giác",
+      singer: "Phan Anh x Low G",
+      path: "./mp3/Tam Giác - Anh Phan ft Low G.mp3",
+      image: "./store/music/thumbnail/tamgiac.jpg",
+    },
+    {
+      name: "Ghệ Đẹp",
+      singer: "Cain x LCKing",
+      path: "./mp3/Cain - Ghệ Đẹp ft LCKing.mp3",
+      image: "./store/music/thumbnail/ghedep.jpg",
+    },
+    {
+      name: "Duyên Âm",
+      singer: "Hoàng Thùy Linh",
+      path: "./mp3/Duyên Âm - Hoàng Thùy Linh.mp3",
+      image: "./store/music/thumbnail/duyenam.jpg",
+    },
+    {
+      name: "Mời Trầu",
+      singer: "Masew",
+      path: "./mp3/Mời Trầu - Masew.mp3",
+      image: "./store/music/thumbnail/moitrau.jpg",
+    },
+    {
+      name: "Người Đi Bao",
+      singer: "TLinh x Low G",
+      path: "./mp3/Người Đi Bao - TLinh x Low G.mp3",
+      image: "./store/music/thumbnail/nguoidibao.jpg",
+    },
+    {
+      name: "Nhất Thân",
+      singer: "Masew",
+      path: "./mp3/Nhất Thân - Masew x Khoi Vu.mp3",
+      image: "./store/music/thumbnail/nhatthan.jpg",
+    },
+    {
+      name: "Ôm Em Lần Cuối",
+      singer: "Nit x SingCukCak",
+      path: "./mp3/Ôm Em Lần Cuối - Nit ft SingCukak.mp3",
+      image: "./store/music/thumbnail/omemlancuoi.jpg",
+    },
+    {
+      name: "Simple Cypher",
+      singer: "Low G",
+      path: "./mp3/Simple Cypher - Low G.mp3",
+      image: "./store/music/thumbnail/simplecypher.jpg",
+    },
+    {
+      name: "Túy Âm",
+      singer: "Masew",
+      path: "./mp3/Túy Âm - Xesi x Masew x Nhatnguyen.mp3",
+      image: "./store/music/thumbnail/tuyam.jpg",
+    },
+  ]
